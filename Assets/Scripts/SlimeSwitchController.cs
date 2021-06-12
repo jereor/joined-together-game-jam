1188582a99b4c186821658ea11b268f36273c59d
jereor <jere.orava@hotmail.com>
2021-06-12 20:34:27 +0300

    Green and blue slime

---
+++ Assets/Scripts/SlimeSwitchController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeSwitchController : MonoBehaviour
+{
+    [Header("Slimes")]
+    [SerializeField] GameObject greenSlime;
+    [SerializeField] GameObject blueSlime;
+    [SerializeField] GameObject currentSlime;
+
+    private void Awake()
+    {
+        currentSlime = greenSlime;
+        currentSlime.GetComponent<PlayerMovement>().enabled = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            SwitchSlimes();
+        }
+    }
+
+    private void SwitchSlimes()
+    {
+        currentSlime.GetComponent<PlayerMovement>().enabled = false;
+        var rb = currentSlime.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector3(0f, rb.velocity.y);
+        var oldSlime = currentSlime;
+
+        Debug.Log(rb.velocity.y);
+
+        if (currentSlime.CompareTag("GreenSlime"))
+            currentSlime = blueSlime;
+        else if (currentSlime.CompareTag("BlueSlime"))
+            currentSlime = greenSlime;
+        currentSlime.GetComponent<PlayerMovement>().enabled = true;
+
+        if (rb.velocity.y == 0)
+            rb.isKinematic = true;
+    }
+}
