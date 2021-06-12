c5141872b5e7e730ef005e96ee4090755b405e62
jereor <jere.orava@hotmail.com>
2021-06-12 13:46:13 +0300

    Player animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Config parameters
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    [SerializeField] LayerMask whatIsGround;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float checkRadius;
+    [SerializeField] int extraJumpsValue;
+
+    // State variables
+    private float _moveInput;
+    private Rigidbody2D _rb;
+    [SerializeField] private bool _isGrounded;
+    private bool _facingRight = true;
+    [SerializeField] private int _extraJumps;
+
+    // Cached references
+    private Animator _animator;
+
+    // Animator states
+    private static readonly int Moving = Animator.StringToHash("Moving");
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _extraJumps = extraJumpsValue;
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        _moveInput = Input.GetAxis("Horizontal");
+        _rb.velocity = new Vector2(_moveInput * speed, _rb.velocity.y);
+
+        if (_facingRight == false && _moveInput > 0)
+            Flip();
+        else if (_facingRight == true && _moveInput < 0)
+            Flip();
+
+        if (_rb.velocity.x != 0)
+            _animator.SetBool(Moving, true);
+        else
+            _animator.SetBool(Moving, false);
+    }
+
+    private void Update()
+    {
+        if (_isGrounded)
+            _extraJumps = extraJumpsValue;
+
+        if (Input.GetKeyDown(KeyCode.Space) && _extraJumps > 0)
+        {
+            _rb.velocity = Vector2.up * jumpForce;
+            _extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && _extraJumps == 0 && _isGrounded == true)
+        {
+            _rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }
